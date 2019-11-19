7eae86cedda54908988bb3497afa1ddd527c0182
Gremlin4544 <alve@mckethen.com>
2019-11-19 15:04:32 -0600

    added Trip file - no func

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/header/Header';
 import CreateAccount from './components/register/CreateAccount';
 import SignIn from './components/login/SignIn';
+import Trip from './components/Trip';
 
 function App() {
   return (
